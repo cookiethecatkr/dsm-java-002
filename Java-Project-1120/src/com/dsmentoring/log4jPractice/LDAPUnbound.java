e2dd1d09113518e0cb88631f0f2e59c848358168
jhlee <jhlee@dsmentoring.com>
2018-11-22 14:48:33 +0900

    11/22 second draft

--- Java-Project-1120/src/com/dsmentoring/log4jPractice/LDAPUnbound.java
+++ Java-Project-1120/src/com/dsmentoring/log4jPractice/LDAPUnbound.java
@@ -1,5 +1,8 @@
 package com.dsmentoring.log4jPractice;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.unboundid.ldap.sdk.LDAPConnection;
 import com.unboundid.ldap.sdk.LDAPException;
 import com.unboundid.ldap.sdk.SearchRequest;
@@ -8,36 +11,33 @@ import com.unboundid.ldap.sdk.SearchScope;
 
 public class LDAPUnbound {
 
-	public static LDAPConnection connectSDK(String ip, Integer port, String id, String pw) throws LDAPException    {
+	private static final Logger LOG = LogManager.getLogger(LDAPUnbound.class);
+	
+	public static LDAPConnection connectSDK(String ip, int port, String id, String pw) throws LDAPException {
 			
 		LDAPConnection ldap = new LDAPConnection(ip,port,id,pw);
-		System.out.println("접속 성공");	
+		LOG.info("접속 성공");	
 		return ldap;
-			
 	}
 	    
 	public static void searchSDK(String filter, LDAPConnection ldap) throws LDAPException {
 			
 		SearchRequest searchRequest = new SearchRequest("c=kr",SearchScope.SUB,filter);
 		SearchResult searchResult = ldap.search(searchRequest);
-		System.out.println(searchResult);
-			
+		LOG.info(searchResult);
 	}
 
 	public static void closeSDK(LDAPConnection ldap) {
 		
 		ldap.close();
-		System.out.println("사요나라");
+		LOG.info("사요나라");
 	}
 	
 	public static void main(String[] args) throws LDAPException {
 		
 		LDAPConnection ldap = connectSDK("192.168.0.60",389,"cn=govmanager","GOVmoi!manager");
-		
 		searchSDK("cn=007이진형001",ldap);
-		
-		//closeSDK(ldap);
+		closeSDK(ldap);
 	}	
-
 } 
 
