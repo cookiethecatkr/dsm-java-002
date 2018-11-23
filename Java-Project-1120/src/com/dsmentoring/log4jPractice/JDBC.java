f4b19b22c7d2b5b9c1ca0bd9a8725dec728c8754
jhlee <jhlee@dsmentoring.com>
2018-11-23 14:45:13 +0900

    branch commit

--- Java-Project-1120/src/com/dsmentoring/log4jPractice/JDBC.java
+++ Java-Project-1120/src/com/dsmentoring/log4jPractice/JDBC.java
@@ -14,23 +14,22 @@ public class JDBC {
 	private static final Logger LOG = LogManager.getLogger(LDAPUnbound.class);
 	
 	public JDBC() {
-		
 	}
 	
-	public static Connection connectDB(String ip, String id, String pw) throws ClassNotFoundException, SQLException {            
+	public Connection connectDB(String ip, String database, String serverTimeZone, String id, String pw) throws ClassNotFoundException, SQLException {            
 		
 		Connection con;
 	    Class.forName("com.mysql.cj.jdbc.Driver"); 
-		con = DriverManager.getConnection("jdbc:mysql://" +ip+"?useSSL=false&serverTimezone=UTC", id, pw);
+		con = DriverManager.getConnection("jdbc:mysql://" + ip + "/" + database+"?serverTimezone=" + serverTimeZone, id, pw);
           
 		LOG.info(con);
 	    
 		return con;
     }	
 
-	public static void select(Connection con, String USRIDX ) throws ClassNotFoundException { 
+	public void select(Connection con, String sql ) throws ClassNotFoundException { 
 	
-	String sql = "SELECT * from test where USRIDX =" + USRIDX;
+	//String sql = "SELECT * from test where USRIDX =" + USRIDX;
 	
 		try(PreparedStatement pstmt = con.prepareStatement(sql)) {
 			ResultSet rs = pstmt.executeQuery(sql);
@@ -46,15 +45,15 @@ public class JDBC {
 		}
 	}	
 	
-	public static void closeJDBC(Connection con) throws SQLException {
+	public void closeJDBC(Connection con) throws SQLException {
 		con.close();
 		LOG.info("짜이찌엔");
 	}
 	
-		public static void main(String[] args) throws ClassNotFoundException, SQLException {
-			
-			Connection con =connectDB("192.168.0.32/SAC","SAC","SAC");
-			select(con,"1");  
-			closeJDBC(con);
-		}
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		JDBC jdbc = new JDBC();	
+		Connection con =jdbc.connectDB("192.168.0.32","SAC","UTC","SAC","SAC");
+		jdbc.select(con,"SELECT * from test where USRIDX = 2");  
+		jdbc.closeJDBC(con);
+	}
 }
--- Java-Project-1120/src/com/dsmentoring/log4jPractice/LDAPUnbound.java
+++ Java-Project-1120/src/com/dsmentoring/log4jPractice/LDAPUnbound.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 
 import com.unboundid.ldap.sdk.LDAPConnection;
 import com.unboundid.ldap.sdk.LDAPException;
-import com.unboundid.ldap.sdk.SearchRequest;
 import com.unboundid.ldap.sdk.SearchResult;
 import com.unboundid.ldap.sdk.SearchScope;
 
@@ -13,32 +12,32 @@ public class LDAPUnbound {
 
 	private static final Logger LOG = LogManager.getLogger(LDAPUnbound.class);
 	
-	public static LDAPConnection connectSDK(String ip, int port, String id, String pw) throws LDAPException {
+	public LDAPConnection connectSDK(String ip, int port, String id, String pw) throws LDAPException {
 			
 		LDAPConnection ldap = new LDAPConnection(ip,port,id,pw);
 		LOG.info("접속 성공");	
 		return ldap;
 	}
 	    
-	public static void searchSDK(String filter, LDAPConnection ldap) throws LDAPException {
+	public SearchResult searchLdap(String baseDn, String filter, LDAPConnection ldap) throws LDAPException {
 			
-		SearchResult searchResult = ldap.search("c=kr",SearchScope.SUB,filter);
-		LOG.info(searchResult);
-		LOG.info(searchResult.getEntryCount());
+		SearchResult searchResult = ldap.search(baseDn,SearchScope.SUB,filter);
 		LOG.info(searchResult.getSearchEntries());
+		System.out.println(" 조회한 결과를 알려드리겠습니다." + searchResult.getSearchEntries());
+		return searchResult;
 	}
 
-	public static void closeSDK(LDAPConnection ldap) {
+	public void closeLdap(LDAPConnection ldap) {
 		
 		ldap.close();
 		LOG.info("사요나라");
 	}
 	
 	public static void main(String[] args) throws LDAPException {
-		
-		LDAPConnection ldap = connectSDK("192.168.0.60",389,"cn=govmanager","GOVmoi!manager");
-		searchSDK("cn=lee",ldap);
-		closeSDK(ldap);
+		LDAPUnbound unbound = new LDAPUnbound();
+		LDAPConnection ldap = unbound.connectSDK("192.168.0.60",389,"cn=govmanager","GOVmoi!manager");
+		unbound.searchLdap("c=kr","cn=lee",ldap);
+		unbound.closeLdap(ldap);
 	}	
 } 
 
